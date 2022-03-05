9226b4d389dacb93bb7835f9e35611f3e20b2b5f
juniq <juniqlim@gmail.com>
2022-03-05 20:40:38 +0900

    Initial commit

---
+++ src/test/java/im/juniq/apicall/ApiCallApplicationTests.java
@@ -0,0 +1,13 @@
+package im.juniq.apicall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ea72c8319e973d76f03f2b2b0c0fa08e8932e0b5
juniq <juniqlim@gmail.com>
2022-04-28 18:10:42 +0900

    feat: 메이븐 중앙저장소에 publish

--- src/main/java/im/juniq/apicall/http/HttpApiCall.java
+++ src/main/java/im/juniq/apicall/http/HttpApiCall.java
@@ -1,5 +1,8 @@
 package im.juniq.apicall.http;
 
+/**
+ * interface
+ */
 public interface HttpApiCall {
     <Q, S> S callApi(HttpRequest<Q, S> request);
     <Q> void callApi(HttpRequestWithoutResponse<Q> response);
